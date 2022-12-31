743bdc1782042bacc770bd14989e1f05b3fe13fa
edwardash <edwardash00@gmail.com>
2022-12-31 16:42:27 +0530

    CORS

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 import dotenv from 'dotenv';
 const app = express();
-dotenv.config();
+// dotenv.config();
 
 
 // app.all('*', (req, res, next) => {
 //     res.header("Access-Control-Allow-Origin", "https://localhost:3000");
 //     next();
 // });
+app.use(cors());
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use(cors());
+
 
 
 app.use('/posts', postRoutes);
